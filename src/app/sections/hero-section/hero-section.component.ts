662791f5a0a43f4b5a7419d1d10f4a27e5f6dc54
EHAB3x <ehabmah6969@gmail.com>
2025-06-05 18:13:37 +0300

    finish scroll down component for hero section

--- src/app/sections/hero-section/hero-section.component.ts
+++ src/app/sections/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroSocialLinksComponent } from "./compoents/hero-social-links/hero-social-links.component";
+import { ScrollDownComponent } from "./compoents/scroll-down/scroll-down.component";
 
 @Component({
   selector: 'app-hero-section',
-  imports: [HeroSocialLinksComponent],
+  imports: [HeroSocialLinksComponent, ScrollDownComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
