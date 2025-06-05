ec79edbd5eae803e39cf8507d1963114b2712cc0
EHAB3x <ehabmah6969@gmail.com>
2025-06-05 18:23:03 +0300

    feat: replace icon with ng-icon component for scroll down button and update imports

--- src/app/sections/hero-section/compoents/scroll-down/scroll-down.component.ts
+++ src/app/sections/hero-section/compoents/scroll-down/scroll-down.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { bootstrapArrowDown } from '@ng-icons/bootstrap-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-scroll-down',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './scroll-down.component.html',
-  styleUrl: './scroll-down.component.scss'
+  styleUrl: './scroll-down.component.scss',
+  viewProviders: [provideIcons({ bootstrapArrowDown })],
 })
-export class ScrollDownComponent {
-
-}
+export class ScrollDownComponent {}
