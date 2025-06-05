458344929eeb327ebf181b96ff9988a37c9d223e
EHAB3x <ehabmah6969@gmail.com>
2025-06-05 18:34:09 +0300

    finish hero section data component

---
+++ src/app/sections/hero-section/compoents/hero-data/hero-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-data',
+  imports: [],
+  templateUrl: './hero-data.component.html',
+  styleUrl: './hero-data.component.scss'
+})
+export class HeroDataComponent {
+
+}
--- src/app/sections/hero-section/hero-section.component.ts
+++ src/app/sections/hero-section/hero-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSocialLinksComponent } from "./compoents/hero-social-links/hero-social-links.component";
 import { ScrollDownComponent } from "./compoents/scroll-down/scroll-down.component";
+import { HeroDataComponent } from "./compoents/hero-data/hero-data.component";
 
 @Component({
   selector: 'app-hero-section',
-  imports: [HeroSocialLinksComponent, ScrollDownComponent],
+  imports: [HeroSocialLinksComponent, ScrollDownComponent, HeroDataComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
