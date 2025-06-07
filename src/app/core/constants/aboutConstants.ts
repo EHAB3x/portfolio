9a52915abc03487db3c91d592087bf4854b96387
EHAB3x <ehabmah6969@gmail.com>
2025-06-07 22:15:19 +0300

    feat: add about constants and interface for experience data

---
+++ src/app/core/constants/aboutConstants.ts
@@ -0,0 +1,19 @@
+import { IAboutConstants } from "../interfaces/iabout-constants";
+
+export const experiences : IAboutConstants[] = [
+  {
+    name: 'Experience',
+    desc: '1 Years Working',
+    svg: 'award',
+  },
+  {
+    name: 'Completed',
+    desc: '+12 Project',
+    svg: 'briefcase-alt',
+  },
+  {
+    name: 'Support',
+    desc: 'Online 24/7',
+    svg: 'support',
+  },
+];
