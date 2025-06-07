75b7d2c9bbd92bbb94f7cfbfb20239a3c3ecfabd
EHAB3x <ehabmah6969@gmail.com>
2025-06-07 23:05:37 +0300

    feat: implement SkillsApiService with getAllSkills method and add unit tests

---
+++ src/app/core/services/skills-api.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  Observable,
+  catchError,
+  delay,
+  of,
+  retryWhen,
+  take,
+  throwError,
+  timeout,
+} from 'rxjs';
+import { ISkills } from '../interfaces/iskills';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SkillsApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  getAllSkills(): Observable<ISkills[]> {
+    return this.httpClient.get<ISkills[]>(`${environment.baseUrl}/skills`).pipe(
+      timeout(5000),
+      retryWhen((errors) => errors.pipe(delay(2000)))
+      // catchError(err => {
+      //   return throwError(() => new Error('Failed to load skills after multiple attempts.'));
+      // })
+    );
+  }
+}
