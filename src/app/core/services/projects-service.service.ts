eb86d951cab77b21e703408873de733aabb7cfdd
EHAB3x <ehabmah6969@gmail.com>
2025-06-08 06:56:57 +0300

    feat: implement ProjectsServiceService to manage project data retrieval

---
+++ src/app/core/services/projects-service.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProjects } from '../interfaces/iprojects';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectsServiceService {
+  constructor(private httpClient: HttpClient) {}
+
+  getAllProjects(): Observable<IProjects[]> {
+    return this.httpClient.get<IProjects[]>(`${environment.baseUrl}/projects`);
+  }
+
+  getAllCategories(): IProjects['category'][] {
+    let cats: IProjects['category'][] = [];
+    this.getAllProjects().subscribe({
+      next: (res) => {
+        cats = res.map((project) => project.category);
+      },
+    });
+    return cats;
+  }
+}
